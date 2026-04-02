85c6fe700ad1c98d4e30fc86daac479df6406813
Decalogue <decalogue80@gmail.com>
2026-04-02 23:30:09 +0800

    update

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import flyllm
+from setuptools import setup, find_packages
+
+
+version = flyllm.__version__
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push origin --tags")
+    os.system("git push --all origin")
+    sys.exit()
+elif sys.argv[-1] == 'publish':
+    os.system("rm -rf dist/ build/ *.egg-info")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    sys.exit()
+elif sys.argv[-1] == 'test':
+    test_requirements = [
+        'pytest',
+        'flake8',
+        'coverage'
+    ]
+    try:
+        modules = map(__import__, test_requirements)
+    except ImportError as e:
+        err_msg = e.message.replace("No module named ", "")
+        msg = "%s is not installed. Install your test requirments." % err_msg
+        raise ImportError(msg)
+    os.system('py.test')
+    sys.exit()
+
+setup(
+    name="flyllm",
+    version=version,
+    author="Decalogue",
+    author_email="decalogue80@gmail.com",
+    description="flyllm",
+    long_description="flyllm",
+    long_description_content_type="text/plain",
+    url="https://github.com/Decalogue/flyllm",
+    packages=find_packages(),
+    python_requires='>=3.10',
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Natural Language :: English",
+        "Natural Language :: Chinese (Simplified)",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Other OS",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
+    ]
+)
